b7bccd03adb4deec1cea7c3a7d8bbb4375bf1ac7
VerifyingUsername <72852459+VerifyingUsername@users.noreply.github.com>
2021-01-24 21:41:11 +0800

    Submission
    Lab17_Submission

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Character : MonoBehaviour
 {
     public float moveSpeed;
     public float jumpForce;
+    public int jumpCount;
 
     public GameObject HealthText;
     public GameObject CoinText;
@@ -17,11 +19,18 @@ public class Character : MonoBehaviour
     private Rigidbody2D rb;
     private Animator animator;
 
+    private AudioSource audioSource;
+    public AudioClip CoinClip;
+    public AudioClip JumpClip;
+    public AudioClip HitClip;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+
+        audioSource = GetComponent<AudioSource>();
 
         HealthText.GetComponent<Text>().text = "Health: " + healthCount;
         CoinText.GetComponent<Text>().text = "Coin: " + coinCount;
@@ -55,10 +64,25 @@ public class Character : MonoBehaviour
             animator.SetFloat("xVelocity", 0);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount < 2)
         {
+            jumpCount += 1;
+            audioSource.clip = JumpClip;
+            audioSource.Play();
             vVelocity = jumpForce;
             animator.SetTrigger("JumpTrigger");
+        }
+
+        // WIn COndition
+        if (coinCount == 3)
+        {
+            SceneManager.LoadScene("WinScene");
+        }
+
+        // Lose COndition
+        if (healthCount <= 0)
+        {
+            SceneManager.LoadScene("LoseScene");
         }
 
         hVelocity = Mathf.Clamp(rb.velocity.x + hVelocity, -5, 5);
@@ -68,15 +92,24 @@ public class Character : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (collision.gameObject.tag == "Ground")
+        {
+            jumpCount = 0;
+        }
+
         if (collision.gameObject.tag == "Enemy")
         {
-            healthCount -= 10;
+            healthCount -= 50;
+            audioSource.clip = HitClip;
+            audioSource.Play();
             HealthText.GetComponent<Text>().text = "Health: " + healthCount;
         }
 
         if (collision.gameObject.tag == "Coin")
         {
             coinCount += 1;
+            audioSource.clip = CoinClip;
+            audioSource.Play();
             Destroy(collision.gameObject);
             CoinText.GetComponent<Text>().text = "Coin: " + coinCount;
         }
